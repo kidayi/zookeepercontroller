f6ff56d98cceb2a82af7d2d90500e988ace93260
duansujie <duansujie>
2016-09-06 20:54:25 +0800

    避免异常

--- ZookeeperController/src/main/java/zookeepercontroller/zkconn/ZookeeperConnection.java
+++ ZookeeperController/src/main/java/zookeepercontroller/zkconn/ZookeeperConnection.java
@@ -1,9 +1,13 @@
 package zookeepercontroller.zkconn;
 
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * User: PageLiu
@@ -13,10 +17,19 @@ import java.io.IOException;
 public class ZookeeperConnection {
     private String connectString;
     private int sessionTimeout;
+    private int zkConnectAwaitMs=1000*2;
     private Watcher watcher;
+    private ZooKeeper zooKeeper  = null;
 
+    public int getZkConnectAwaitMs() {
+		return zkConnectAwaitMs;
+	}
 
-    public String getConnectString() {
+	public void setZkConnectAwaitMs(int zkConnectAwaitMs) {
+		this.zkConnectAwaitMs = zkConnectAwaitMs;
+	}
+
+	public String getConnectString() {
         return connectString;
     }
 
@@ -39,11 +52,36 @@ public class ZookeeperConnection {
     public void setWatcher(Watcher watcher) {
         this.watcher = watcher;
     }
-    ZooKeeper zooKeeper  = null;
+    
+    
     public ZooKeeper createZookeeper() throws IOException {
-        zooKeeper = new ZooKeeper(connectString,sessionTimeout,watcher);
+    	final CountDownLatch connectedSignal = new CountDownLatch(1);
+		try {
+				zooKeeper = new ZooKeeper(connectString,sessionTimeout, new Watcher() {
+				public void process(WatchedEvent event) {
+					System.out.println("createZookeeper:" + event.getPath() 
+							+ "," + event.getType() + "," + event.getState());
+					if (event.getState() == KeeperState.SyncConnected) {
+				          connectedSignal.countDown();
+				    }
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+			return zooKeeper;
+		}
+		
+		try {
+			connectedSignal.await(zkConnectAwaitMs,TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
         return zooKeeper;
     }
+    
+    
+    
     public void close() throws InterruptedException {
         if(zooKeeper!=null){
             zooKeeper.close();
